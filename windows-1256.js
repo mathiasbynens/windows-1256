305d4585caa4627f4e7602cae419286a15ed577c
Mathias Bynens <mathias@qiwi.be>
2014-05-15 15:53:13 +0200

    Tag the v0.1.2 release

--- windows-1256.js
+++ windows-1256.js
@@ -1,4 +1,4 @@
-/*! http://mths.be/windows-1256 v0.1.1 by @mathias | MIT license */
+/*! http://mths.be/windows-1256 v0.1.2 by @mathias | MIT license */
 ;(function(root) {
 
 	// Detect free variables `exports`.
@@ -120,7 +120,7 @@
 			'windows-1256',
 			'x-cp1256'
 		],
-		'version': '0.1.1'
+		'version': '0.1.2'
 	};
 
 	// Some AMD build optimizers, like r.js, check for specific condition patterns
