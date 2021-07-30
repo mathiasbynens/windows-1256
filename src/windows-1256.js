213408bb37ac64e4737b7cc7a01a666564dfa6bb
Mathias Bynens <mathias@qiwi.be>
2021-07-30 10:51:20 +0200

    Set up automated releases

--- src/windows-1256.js
+++ src/windows-1256.js
@@ -1,22 +1,6 @@
 /*! https://mths.be/windows-1256 v<%= version %> by @mathias | MIT license */
 ;(function(root) {
 
-	// Detect free variables `exports`.
-	var freeExports = typeof exports == 'object' && exports;
-
-	// Detect free variable `module`.
-	var freeModule = typeof module == 'object' && module &&
-		module.exports == freeExports && module;
-
-	// Detect free variable `global`, from Node.js/io.js or Browserified code,
-	// and use it as `root`.
-	var freeGlobal = typeof global == 'object' && global;
-	if (freeGlobal.global === freeGlobal || freeGlobal.window === freeGlobal) {
-		root = freeGlobal;
-	}
-
-	/*--------------------------------------------------------------------------*/
-
 	var object = {};
 	var hasOwnProperty = object.hasOwnProperty;
 	var stringFromCharCode = String.fromCharCode;
@@ -33,7 +17,7 @@
 			return '&#' + codePoint + ';';
 		}
 		// Else, `mode == 'fatal'`.
-		throw Error();
+		throw new Error();
 	};
 
 	// https://encoding.spec.whatwg.org/#single-byte-decoder
@@ -113,32 +97,12 @@
 	};
 
 	var windows1256 = {
-		'encode': encode,
-		'decode': decode,
-		'labels': <%= labels %>,
-		'version': '<%= version %>'
+		encode: encode,
+		decode: decode,
+		labels: <%= labels %>,
+		version: '<%= version %>'
 	};
 
-	// Some AMD build optimizers, like r.js, check for specific condition patterns
-	// like the following:
-	if (
-		typeof define == 'function' &&
-		typeof define.amd == 'object' &&
-		define.amd
-	) {
-		define(function() {
-			return windows1256;
-		});
-	}	else if (freeExports && !freeExports.nodeType) {
-		if (freeModule) { // in Node.js, io.js or RingoJS v0.8.0+
-			freeModule.exports = windows1256;
-		} else { // in Narwhal or RingoJS v0.7.0-
-			for (var key in windows1256) {
-				windows1256.hasOwnProperty(key) && (freeExports[key] = windows1256[key]);
-			}
-		}
-	} else { // in Rhino or a web browser
-		root.windows1256 = windows1256;
-	}
+	module.exports = windows1256;
 
 }(this));
